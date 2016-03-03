93edd95012c44c0a02688728b463ac5221df1826
zcszcs <hnhkzcs@163.com>
2016-03-03 10:04:33 +0800

    test

--- git.bash
+++ git.bash
@@ -3,6 +3,7 @@ git config --global user.email "hnhkzcs@163.com"
 git config --global user.name "zcsLab"
 
 #克隆文件夹(仓库)到当前目录
+#要建立仓库才可以push,add和commit是把add的内容提交到仓库里
 git clone path
 git clone username@host:path
 
