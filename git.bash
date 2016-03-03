b05439d20af68198ee915bf7f40666dd781c93fa
zcszcs <hnhkzcs@163.com>
2016-03-03 09:57:34 +0800

    :q

---
+++ git.bash
@@ -0,0 +1,16 @@
+#
+git config --global user.email "hnhkzcs@163.com"
+git config --global user.name "zcsLab"
+
+#克隆文件夹(仓库)到当前目录
+git clone path
+git clone username@host:path
+
+#
+git remote add origin https://github.com/zcszcs/learningTest.git
+git push origin master
+git push
+
+#
+git pull
+
