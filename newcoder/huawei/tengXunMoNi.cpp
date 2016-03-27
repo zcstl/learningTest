0934e94982d4ab77c690dd071f27e6dc5e21cae3
zcszcs <hnhkzcs@163.com>
2016-03-27 23:35:51 +0800

    test

--- newcoder/huawei/tengXunMoNi.cpp
+++ newcoder/huawei/tengXunMoNi.cpp
@@ -10,4 +10,61 @@
 
 /**
     大数相乘
+    模仿两个数相乘的规则，被乘数为两个数中较大者
 **/
+#include <iostream>
+#include <string>
+using namespace std;
+
+void twoNumAdd(string& a, string& b);
+
+int main(){
+    //
+    string a, b;
+    cin>>a>>b;
+    //
+    unsigned char aSize=a.size(), bSize=b.size();
+    for(int i(0); i<aSize; ++i)
+        if(a[i]<'0' && a[i]>'9'){
+            cout<<"Bad input!"<<endl;
+            return -1;
+        }
+    for(int i(0); i<bSize; ++i)
+        if(b[i]<'0' && b[i]>'9'){
+            cout<<"Bad input!"<<endl;
+            return -1;
+        }
+    //
+    int maxLen(0), strLen(0), mutiTimes(0);
+    if(aSize>bSize)
+        maxLen=aSize, strLen=aSize*2, mutiTimes=bSize;
+    else
+        maxLen=bSize, strLen=bSize*2, mutiTimes=aSize, a.swap(b);
+
+    //a*b
+    string tmp(strLen, '0'), res(strLen, '0'), tmpTmp=tmp;
+    for(int i(mutiTimes-1); i>=0; --i){
+        int pushNum(0), s1(0), s2(0), xj(0);
+        for(int j(maxLen-1); j>=0; --j){
+            s1=a[j]-'0', s2=b[i]-'0', xj=s1*s2;
+            //cout<<maxLen+j+(i-mutiTimes+1)<<endl;
+            //cout<<(tmp[maxLen+j+(i-mutiTimes+1)]=(xj+pushNum)%10+'0')<<endl;
+            tmp[maxLen+j+(i-mutiTimes+1)]=(xj+pushNum)%10+'0';
+            pushNum=(xj+pushNum)/10;
+        }
+        tmp[maxLen-1-(mutiTimes-1-i)]=pushNum+'0';
+        //cout<<tmp<<endl;
+        twoNumAdd(res, tmp);
+        tmp=tmpTmp;
+    }
+    cout<<res.substr(res.rfind('0')+1)<<endl;
+}
+
+void twoNumAdd(string& a, string& b){
+    int pushNum(0), s1(0), s2(0), xj(0);
+    for(int i(a.size()-1); i>=0; --i){
+        s1=a[i]-'0', s2=b[i]-'0', xj=s1+s2;
+        a[i]=(xj+pushNum)%10+'0';
+        pushNum=(xj+pushNum)/10;
+    }
+}
