6213b0f5df35d4cca7d0b3c5884a0db02f3d0c11
zcszcs <hnhkzcs@163.com>
2016-04-05 23:39:38 +0800

    test

--- newcoder/huawei/tengXunMoNi.cpp
+++ newcoder/huawei/tengXunMoNi.cpp
@@ -1,10 +1,81 @@
 /**
+    0-1背包问题
+    DP，递推填表
+
+    val[]表示物品价值，wgt[]表示物品重量，重量为0的物品价值为0
+    arr[i][j]表示背包重量为i且只能选1到j个物品时的最价值
+
+    1.若是部分背包问题，即物品按计量单位算，则贪心解决
+    2.若问法是必须装满背包的前提下，则0-1背包问题的初始化为负无穷
+**/
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define nums 3
+#define totalWeigth 5
+
+int main(){
+    //加上0  编程更方便
+    int val[nums+1]={0, 60, 100, 120};
+    int wgt[nums+1]={0, 1, 2, 3};
+    int arr[totalWeigth+1][nums+1]={0};
+
+    for(int i(1); i<=totalWeigth; ++i)
+        for(int j(1); j<=nums; ++j)
+            if(wgt[j]>i)
+                arr[i][j]=arr[i][j-1];
+            else
+                arr[i][j]=max(arr[i][j-1], arr[i-wgt[j]][j-1]+val[j]);
+    cout<<arr[totalWeigth][nums]<<endl;
+}
+*/
+
+/**
+    bitset的使用
+**/
+
+
+/**
+    编程之美2.1: 判断unsigned char变量二进制表示中1的个数
+    思路:
+    1.比较low的思路:循环吗模2取余并除2, 计算余数为1的个数
+    2.判断低位是否为1(& 0x1)然后右移1位(>>1),循环更新num直到为0停止
+    3.查表法,统计256个数字的1的个数建表,典型的空间换时间,,,
+
+    >>运算符的操作数是右值,~运算符的操作数也是右值
+**/
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countOne(unsigned char a);
+
+int main(){
+    unsigned char a(8);
+    cout<<countOne(a)<<endl;
+}
+int countOne(unsigned char a){
+    int num(0);
+    //while(a>=1)
+    while(a){
+        num+=a&0x1;
+        a>>=1;
+    }
+    return num;
+}
+*/
+
+/**
     输入一个字符串，可以去掉某些字符，求最大回文字符串长度
     1.当时问题：完全没思路
     1.2.后来发现可以转化为与逆序列的最长公共子序列问题，但木脑袋没做过LCS，当时也想不出来
 
     2.经典DP案例LCS，要点是从最后一对字符分析开始，相同和不相同情况下入手
 **/
+/*
 #include <iostream>
 #include <string>
 using namespace std;
@@ -29,7 +100,7 @@ int getLCS(char* a, int al, char* b, int bl){
     else
         return max(getLCS(a, al, b, bl-1), getLCS(a, al-1, b, bl));
 }
-
+*/
 
 /**
     打印蛇形矩阵
